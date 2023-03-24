737ae78763c9c7bac34ff2f4fdb651d7589e91a5
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-24 14:31:42 +0300

    Done With Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the main node with id 'main'
+document.querySelector('#main').remove();
+
+// Create a new h1 node with id 'victory' and the message "YOUR-NAME is the champion"
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader);
+
+
+
+
